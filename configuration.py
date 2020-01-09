128970ed095e5a73892de7f8bb06ba993e37039e
uceert0 <58261816+uceert0@users.noreply.github.com>
2020-01-09 13:01:30 +0000

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,8 @@
+FACE_DET_PATH = 'functions/data/haarcascade_frontalface_default.xml'
+
+DB_PATH_INIT = r'C:\\Users\romeo\Desktop\romanos\codebase\Datasets\initial'
+DB_PATH_PREPRO = r'C:\\Users\romeo\Desktop\romanos\codebase\Datasets\generated'
+DB_PATH_FEATURES = r'C:\\Users\romeo\Desktop\romanos\codebase\Datasets\features'
+
+TASK2COL_CELEBA = {'file_name': 0, 'gender': 2, 'smiling': 3}
+TASK2COL_CARTOON = {'file_name': 0, 'face_shape': 2, 'eye_colour': 1}
